9026612e6c49e45d67761e1230acd03c3e24a4da
willy229liu <50225055+willy229liu@users.noreply.github.com>
2025-12-08 15:21:44 +0000

    Deploying to gh-pages from @ willy229liu/willy229liu.github.io@b8eaf78fb29835b608e8e75796c32e3511d1cc3e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -402,44 +402,19 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-10",
-          title: 'project 10',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/10_project/";
-            },},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/11_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/12_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/13_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/14_project/";
-            },},{id: "projects-pip-the-black-hearted-traffic-chasing-promo-machine",
+          section: "News",},{id: "projects-pip-the-black-hearted-traffic-chasing-promo-machine",
           title: 'PIP: The Black-hearted Traffic-Chasing Promo Machine',
           description: "A RogueLite strategy game combining slot machine mechanics with entertainment industry satire.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_PIP/";
-            },},{id: "projects-huawei-codecraft-algorithm-contest",
-          title: 'Huawei CodeCraft Algorithm Contest',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-huawei-codecraft-contest-solution",
+          title: 'Huawei CodeCraft Contest Solution',
+          description: "I won an award in a national algorithm contest",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_code_craft/";
-            },},{id: "projects-remove-lua-debug-info",
-          title: 'Remove Lua Debug Info',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-lua-source-code-memory-optimization",
+          title: 'Lua Source Code Memory Optimization',
+          description: "a solution that minimizes memory of lua codes.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_lua_memory/";
             },},{id: "projects-irbl",
@@ -447,31 +422,6 @@ ninja.data = [{
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_irbl/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-cv',
         title: 'CV',
