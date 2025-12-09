2f657cdb83cc34d3cc40e7f4ddaa0cfabbe4420c
willy229liu <50225055+willy229liu@users.noreply.github.com>
2025-12-09 04:01:12 +0000

    Deploying to gh-pages from @ willy229liu/willy229liu.github.io@f19c6b8a2077a244b0afbb8948044a5bea064ab7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,13 +4,13 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
   },{id: "nav-portfolio",
-          title: "portfolio",
+          title: "Portfolio",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
